01dfe25ce6d2d335ab56f1423505f81ac51d9eaa
echochao <echochao@126.com>
2023-03-30 15:20:17 +0800

    初始化项目，引入Element和axios

---
+++ src/api/requests.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+//引入进度条
+import nprogress from 'nprogress';
+//引入相关进度条的样式
+import "nprogress/nprogress.css";
+
+const requests = axios.create({
+    //baseURL: localStorage.getItem("baseURL"),
+    baseURL: "http://127.0.0.1:8080",
+    timeout: 10 * 1000
+})
+
+// request 拦截器
+requests.interceptors.request.use(config => {
+    config.headers['Content-Type'] = 'application/json;charset=utf-8';
+    nprogress.start();
+    return config
+}, error => {
+    return Promise.reject(error)
+});
+
+// response 拦截器
+requests.interceptors.response.use(
+    response => {
+        nprogress.done();
+        return response.data;
+    },
+    error => {
+        nprogress.done();
+        console.log('error=', error) // for debug
+        return Promise.reject(error)
+    }
+)
+export default requests
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+    <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+    />
+  </el-select>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
